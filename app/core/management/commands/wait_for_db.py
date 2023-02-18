8d6b94c0cd8efe19dd321d30ac7e51ccbb348738
Najah7 <najahmatsuo7@gmail.com>
2023-02-18 16:18:07 +0900

    ✨  db用のコマンドを作成開始

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,13 @@
+"""
+Django command to wait for the database to be available.
+"""
+
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
