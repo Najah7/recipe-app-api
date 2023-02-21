6b7a84182776d5c4b5f907077ec28bd69f89fedb
Najah7 <najahmatsuo7@gmail.com>
2023-02-22 03:50:19 +0900

    ✨ ユーザ作成用のAPIのルーティング

---
+++ app/user/urls.py
@@ -0,0 +1,13 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+# 逆引きで使う用
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
